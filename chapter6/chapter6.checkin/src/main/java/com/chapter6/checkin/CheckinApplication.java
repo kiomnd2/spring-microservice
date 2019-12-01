80da135ec8437658150490da660b7f59e0a3b28f
김형익 <kiomnd2@naver.com>
2019-12-02 00:31:11 +0900

    chapter6. checkin

---
+++ chapter6/chapter6.checkin/src/main/java/com/chapter6/checkin/CheckinApplication.java
@@ -0,0 +1,41 @@
+package com.chapter6.checkin;
+
+import com.chapter6.checkin.entity.CheckInRecord;
+import com.chapter6.checkin.repository.CheckinRepository;
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class CheckinApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(CheckinApplication.class);
+
+    @Autowired
+    CheckinRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckinApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+        CheckInRecord record = new CheckInRecord("Franc", "Gean","28A",new Date(),"BF101","22-JAN-18",1);
+
+        CheckInRecord result  = repository.save(record);
+        logger.info("checked in successfully ..." + result);
+
+
+
+        logger.info("Looking to load checkedIn record...");
+        logger.info("Result: " + repository.findById(result.getId()));
+
+
+    }
+}
