aa10bff4a7b81d96863d6f7d33736c0793836afa
김형익 <kiomnd2@naver.com>
2019-12-02 01:09:11 +0900

    chapter6. fares

---
+++ chapter6/chapter6.fares/src/main/java/com/chapter6/fares/controller/FaresController.java
@@ -0,0 +1,27 @@
+package com.chapter6.fares.controller;
+
+import com.chapter6.fares.component.FaresComponent;
+import com.chapter6.fares.entity.Fare;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/fares")
+public class FaresController {
+    FaresComponent faresComponent;
+
+    @Autowired
+    FaresController(FaresComponent faresComponent)
+    {
+        this.faresComponent = faresComponent;
+    }
+
+    Fare getFare(@RequestParam(value="flightNumber") String flightNumber, @RequestParam(value="flightDate") String flightDate )
+    {
+        return faresComponent.getFare(flightNumber,flightDate);
+    }
+}
